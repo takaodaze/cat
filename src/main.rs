e78b305a58de11c661244896f16b51231fae7c1d
takaodaze <gin2798@gmail.com>
2022-03-02 02:48:49 +0900

    installed clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use clap::Parser;
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+    file_name: String,
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", args);
 }
